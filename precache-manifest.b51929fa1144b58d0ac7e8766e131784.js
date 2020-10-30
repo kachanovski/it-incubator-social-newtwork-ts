357eb2353b7e914f936972b3e6f71f0b96bd823d
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-10-31 00:45:36 +0300

    Updates

---
+++ precache-manifest.b51929fa1144b58d0ac7e8766e131784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f40a510af37cbb5998996bc3a30d62c",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "6761f0836b94d929d979",
+    "url": "/social-network/static/css/main.2454c902.chunk.css"
+  },
+  {
+    "revision": "69496f7b64344e60512c",
+    "url": "/social-network/static/js/2.a2f00008.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/social-network/static/js/2.a2f00008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6761f0836b94d929d979",
+    "url": "/social-network/static/js/main.838bb7ee.chunk.js"
+  },
+  {
+    "revision": "26e28dbee3bd76ed1c63",
+    "url": "/social-network/static/js/runtime-main.ec619e01.js"
+  },
+  {
+    "revision": "a2c9806034cf09a7fd362228445f9621",
+    "url": "/social-network/static/media/loginBackground.a2c98060.jpg"
+  }
+]);
